b6c80331505bd1796780fbbb68faf0c4d32aba68
Woody <2496802850@qq.com>
2024-12-05 01:35:25 +0800

    1、添加了请求课程表的任务处理函数，提升了数据回传的效率。

--- ExamSystemServer/ExamSystemServer/command.cpp
+++ ExamSystemServer/ExamSystemServer/command.cpp
@@ -4,6 +4,7 @@ CCommand::CCommand()
 {
 	this->m_threadPool = new CThreadPool(4);
 	pthread_mutex_init(&this->m_mutex,nullptr);
+	pthread_mutex_init(&this->m_mutex2,nullptr);
 	struct arr {
 		int cmd;
 		FUNC func;
@@ -11,6 +12,7 @@ CCommand::CCommand()
 	{
 		{0,&CCommand::getHeadPicture},
 	    {1,&CCommand::upLoadHeadPicture},
+	    {2,&CCommand::getClassTbaleIcon},
 	    {-1,nullptr}
 	};
 
@@ -27,6 +29,117 @@ CCommand::~CCommand()
 		delete this->m_threadPool;
 	}
 }
+
+void CCommand::getClassTbaleIcon(char* filePath, int sockClient, int epfd, int dataLenght)
+{
+	printf("path:%s\r\n", filePath);
+	//将任务函数添加到线程池中,等执行任务完毕关闭套接字，并且将文件描述符从fd中移除,并且需要释放传入的数据部分
+	FILE* pFile = fopen(filePath, "rb+");
+	if (pFile == nullptr)
+	{
+		delete filePath;
+		fclose(pFile);
+		close(sockClient);
+		pthread_mutex_lock(&this->m_mutex);
+		epoll_ctl(epfd, EPOLL_CTL_DEL, sockClient, NULL);
+		pthread_mutex_unlock(&this->m_mutex);
+		return;
+	}
+	fseek(pFile, 0, SEEK_END);
+	off_t fileSize = ftello64(pFile);
+	fseek(pFile, 0, SEEK_SET);
+
+	//先向客户端发送文件大小
+	long long temp = fileSize;
+	char* strFileSize = new char[8];
+	memset(strFileSize,'\0',sizeof(char) * 8);
+	memcpy(strFileSize,&temp,sizeof(temp));
+
+	//完全发送出这八个字节
+	long long  sendCount = 8;
+	long long alReadySend = 0;
+	while (alReadySend < sendCount)
+	{
+		ssize_t size = write(sockClient, strFileSize + alReadySend, sendCount - alReadySend);
+		if (size <= 0)
+		{
+			break;
+		}
+		alReadySend += size;
+	}
+	delete[] strFileSize;
+
+	//进行读文件，将文件的所有
+	char* fileData = new char[fileSize];
+	memset(fileData,'\0',sizeof(char) * fileSize);
+	size_t ret = fread(fileData, 1, fileSize, pFile);
+	fclose(pFile);
+
+	sendCount = fileSize;
+	alReadySend =  0;
+	while (alReadySend < sendCount)
+	{
+		ssize_t size = write(sockClient, fileData + alReadySend, sendCount - alReadySend);
+		if (size <= 0)
+		{
+			break;
+		}
+		alReadySend += size;
+	}
+	delete[] fileData;
+
+
+	//char* data = new char[1024 * 1024 * 2]; 
+	//memset(data,'\0',sizeof(char)* 1024 * 1024 * 2);
+
+	/*char* data = new char[1024 * 500];
+	memset(data, '\0', sizeof(char) * 1024 * 500);
+	size_t ret = fread(data, 1, fileSize, pFile);
+	//this->Dump(data,ret);
+	printf("read file size:%d\r\n", ret);
+	fclose(pFile);
+	//封包
+	char* packet = new char[2 + 4 + 2 + (1024 * 500) + 1];
+	char* p = packet;
+	short head = 0xFEFF;
+	unsigned int length = fileSize;
+	short cmd = 0;
+	printf("data length:%d\r\n", length);
+	memcpy(p, &head, sizeof(head));
+	p += sizeof(short);
+	memcpy(p, &length, sizeof(length));
+	p += sizeof(unsigned int);
+	memcpy(p, &cmd, sizeof(cmd));
+	p += sizeof(short);
+	memcpy(p, data, length);
+	p += length;
+
+	//pthread_mutex_lock(&this->m_mutex2);
+	//发送数据包
+	long long  sendCount = 2 + 4 + 2 + (1024 * 500) + 1;
+	long long alReadySend = 0;
+	while (true)
+	{
+		ssize_t size = write(sockClient, packet + alReadySend, sendCount - alReadySend);
+		if (size <= 0)
+		{
+			break;
+		}
+		alReadySend += size;
+	}
+	printf("file:%s  send size:%d\r\n", filePath, alReadySend);
+	if (alReadySend < 0)
+	{
+		printf("send Error!\r\n");
+	}
+	delete[] filePath;*/
+	close(sockClient);
+	//pthread_mutex_unlock(&this->m_mutex2);
+	pthread_mutex_lock(&this->m_mutex);
+	epoll_ctl(epfd, EPOLL_CTL_DEL, sockClient, NULL);
+	pthread_mutex_unlock(&this->m_mutex);
+}
+
 
 void CCommand::upLoadHeadPicture(char* pData, int sockClient, int epfd,int dataLength)
 {
@@ -87,6 +200,8 @@ void CCommand::upLoadHeadPicture(char* pData, int sockClient, int epfd,int dataL
 */
 void CCommand::getHeadPicture(char* filePath, int sockClient, int epfd,int dataLength)
 {
+	
+	printf("path:%s\r\n",filePath);
 	//将任务函数添加到线程池中,等执行任务完毕关闭套接字，并且将文件描述符从fd中移除,并且需要释放传入的数据部分
 	FILE* pFile = fopen(filePath,"rb+");
 	if (pFile == nullptr)
@@ -108,7 +223,9 @@ void CCommand::getHeadPicture(char* filePath, int sockClient, int epfd,int dataL
 	char* data = new char[1024 * 500]; 
 	memset(data,'\0',sizeof(char)* 1024 * 500);
 	size_t ret =  fread(data,1, fileSize,pFile);
+	//this->Dump(data,ret);
 	printf("read file size:%d\r\n",ret);
+	fclose(pFile);
 	//封包
 	char* packet = new char[2 + 4 + 2 + (1024 * 500) + 1];
 	char* p = packet;
@@ -125,6 +242,7 @@ void CCommand::getHeadPicture(char* filePath, int sockClient, int epfd,int dataL
 	memcpy(p,data,length);
 	p += length;
 
+	//pthread_mutex_lock(&this->m_mutex2);
 	//发送数据包
 	long long  sendCount = 2 + 4 + 2 + (1024 * 500) + 1;
 	long long alReadySend = 0;
@@ -137,13 +255,14 @@ void CCommand::getHeadPicture(char* filePath, int sockClient, int epfd,int dataL
 		}
 		alReadySend += size;
 	}
-	printf("send size:%d\r\n",alReadySend);
+	printf("file:%s  send size:%d\r\n",filePath,alReadySend);
 	if (alReadySend < 0)
 	{
 		printf("send Error!\r\n");
 	}
 	delete[] filePath;
 	close(sockClient);
+	//pthread_mutex_unlock(&this->m_mutex2);
 	pthread_mutex_lock(&this->m_mutex);
 	epoll_ctl(epfd, EPOLL_CTL_DEL,sockClient, NULL);
 	pthread_mutex_unlock(&this->m_mutex);
--- ExamSystemServer/ExamSystemServer/command.h
+++ ExamSystemServer/ExamSystemServer/command.h
@@ -10,13 +10,32 @@ class CCommand
 public:
 	CCommand();
 	~CCommand();
+	void getClassTbaleIcon(char* filePath,int sockClient, int epfd, int dataLenght);
 	void getHeadPicture(char* filePath,int sockClient,int epfd,int dataLenght);//根据路径读取本地头像图片信息，并且将内容发送
 	void upLoadHeadPicture(char* pData,int sockClient,int epfd,int dataLenght); //客户端上传头像信息到服务器磁盘进行存储
 	int Excute(int cmd,char* data,int sockClient,int epfd,int dataLength); 
 public:
 	CThreadPool* m_threadPool;
+	void Dump(const char* Data, size_t nSize)  //打印输出测试设计的包的数据是什么
+	{
+		std::string strOut;  //用于存储整个包的数据的结果
+		//strOut.resize(nSize);
+		for (size_t i = 0; i < nSize; i++)
+		{
+			char buf[8] = "";
+			if (i > 0 && (i % 16 == 0))
+			{
+				strOut += '\n';
+			}
+			snprintf(buf, sizeof(buf), "%02X", Data[i] & 0xFF);  //%02X的意思是 写入的十六进制占两位，不足位是填充前导零 
+			strOut += buf;
+		}
+		strOut += "\n";
+		printf("%s",strOut.c_str()); //用于输出调试的字符串，类似于QDebug
+	}
 private:
 	pthread_mutex_t m_mutex;
+	pthread_mutex_t m_mutex2;
 	typedef void (CCommand::* FUNC)(char*,int,int,int);
 	std::map<int, FUNC> m_funcMap;
 };
